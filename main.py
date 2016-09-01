827beb696475df78aaca538dc199af8c4cb95335
DragonDope <DragonDope@ist-willig.de>
2016-09-02 00:31:40 +0200

    Update 2016-09-02

--- main.py
+++ main.py
@@ -6,61 +6,76 @@ import RPi.GPIO as GPIO
 import email_alarm
 from ConfigParser import SafeConfigParser
 
-def init():
 
-	# Einstellungen abrufen und setzen
-	#---------------------------------
+# Einstellungen abrufen und setzen
+#---------------------------------
+config = SafeConfigParser()
+config.read('./alert.conf')
 
-        config = SafeConfigParser()
-        config.read('./alert.conf')
+email = config.getboolean('alert', 'email')			# Soll eine Alarm- E-Mail versendet werden?
+telefon = config.getboolean('alert', 'telefon')			# Soll ein Alarmanruf ausgeführt werden
 
-	email = config.getboolean('alert', 'email')			# Soll eine Alarm- E-Mail versendet werden?
-	telefon = config.getboolean('alert', 'telefon')			# Soll ein Alarmanruf ausgeführt werden
+IN_01 = config.getint('hardware', 'pin_IN_01')
+IN_02 = config.getint('hardware', 'pin_IN_02')
+IN_03 = config.getint('hardware', 'pin_IN_03')
+IN_04 = config.getint('hardware', 'pin_IN_04')
 
-	IN_01 = config.getint('hardware', 'pin_IN_01')
-	IN_02 = config.getint('hardware', 'pin_IN_02')
-	IN_03 = config.getint('hardware', 'pin_IN_03')
-	IN_04 = config.getint('hardware', 'pin_IN_04')
+#Schlagwort Dictionary
 
-	#Schlagwort Dictionary
+name_IN_01 = config.get('name', 'name_IN_01')
+name_IN_02 = config.get('name', 'name_IN_02')
+name_IN_03 = config.get('name', 'name_IN_03')
+name_IN_04 = config.get('name', 'name_IN_04')
 
-	name_IN_01 = config.get('name', 'name_IN_01')
-	name_IN_02 = config.get('name', 'name_IN_02')
-	name_IN_03 = config.get('name', 'name_IN_03')
-	name_IN_04 = config.get('name', 'name_IN_04')
+OUT_01 = config.getint('hardware', 'pin_OUT_01')
+OUT_02 = config.getint('hardware', 'pin_OUT_02')
+OUT_03 = config.getint('hardware', 'pin_OUT_03')
 
-	OUT_01 = config.getint('hardware', 'pin_OUT_01')
-	OUT_02 = config.getint('hardware', 'pin_OUT_02')
-	OUT_03 = config.getint('hardware', 'pin_OUT_03')
+logfile = config.get('alert', 'logfile')
 
 
-        # GPIO initialisieren
-        #---------------------
-        GPIO.setmode(GPIO.BOARD)       					# PIN Nummerierung per P1 RPi Board
-        GPIO.setwarnings(False)         				# Warnung mehrfach Initialisierung deaktivieren
+# GPIO initialisieren
+#---------------------
+GPIO.setmode(GPIO.BOARD)       					# PIN Nummerierung per P1 RPi Board
+GPIO.setwarnings(False)         				# Warnung mehrfach Initialisierung deaktivieren
 
-	GPIO.setup(IN_01, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)		# GPIO PIN einstellen
-	GPIO.setup(IN_02, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)		# GPIO PIN einstellen
-	GPIO.setup(IN_03, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
-	GPIO.setup(IN_04, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
-	GPIO.setup(OUT_01, GPIO.OUT)					# GPIO PIN einstellen
-	GPIO.setup(OUT_02, GPIO.OUT)                                    # GPIO PIN einstellen
-	GPIO.setup(OUT_03, GPIO.OUT)                                    # GPIO PIN einstellen
+GPIO.setup(IN_01, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)		# GPIO PIN einstellen
+GPIO.setup(IN_02, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)		# GPIO PIN einstellen
+GPIO.setup(IN_03, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
+GPIO.setup(IN_04, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
+GPIO.setup(OUT_01, GPIO.OUT)					# GPIO PIN einstellen
+GPIO.setup(OUT_02, GPIO.OUT)                                    # GPIO PIN einstellen
+GPIO.setup(OUT_03, GPIO.OUT)                                    # GPIO PIN einstellen
 
 
 def main():
 	
-	init()								# Unterprogramm init aufrufen
-	while(true):							#Loop
+
+        GPIO.add_event_detect(IN_01, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
+        GPIO.add_event_detect(IN_02, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
+        GPIO.add_event_detect(IN_03, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
+        GPIO.add_event_detect(IN_04, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
+
+	while(True):							#Loop
 		# Zusänder der Eingänge abrufen
 		#------------------------------
 		zeit = time.strftime("%Y-%m-%d;%X")				# Aktuelle Systemzeit einlesen
-		state_01 = GPIO.input(IN_01)					# GPIO Status einlesen
-		state_02 = GPIO.input(IN_02)					# GPIO Status einlesen
-		state_03 = GPIO.input(IN_03)					# GPIO Status einlesen
-		state_04 = GPIO.input(IN_04)                                    # GPIO Status einlesen
+		
+		if GPIO.event_detected(IN_01):					# Auslöser
+			save_log(name_IN_01)					# Log eintragen
+                if GPIO.event_detected(IN_02):                                  # Auslöser
+                        save_log(name_IN_02)                                    # Log eintragen
+                if GPIO.event_detected(IN_03):                                  # Auslöser
+                        save_log(name_IN_03)                                    # Log eintragen
+                if GPIO.event_detected(IN_04):                                  # Auslöser
+                        save_log(name_IN_04)                                    # Log eintragen
 
 		time.sleep(0.5)							#Wartezeit bis die Schleifen ein weiters mal durchlaufen wird
+
+
+def save_log(name):
+	file = open(logfile,"a")						# Datei öffnen
+	file.write(str(zeit) + ";" + name + "\n")				# Daten in Datei speichern
 
 
 def Result():
@@ -72,15 +87,14 @@ def Result():
 	print(str(zeit)  + " ; " + name_IN_03 + " ; " + str(state_03) + " (Pull-DOWN)")            # Bildschirmausgabe
 	print(str(zeit)  + " ; " + name_IN_04 + " ; " + str(state_04) + " (Pull-DOWN)")            # Bildschirmausgabe
 
-
 	# Daten in Datei speichern
 	#--------------------------
-	file = open("alert.csv","a")							# Datei öffnen
-	file.write(str(zeit) + " ; " + name_IN_01 + " ; " + str(state_01) + " (Pull-DOWN)\n")	# Daten in Datei speichern
-	file.write(str(zeit) + " ; " + name_IN_02 + " ; " + str(state_02) + " (Pull-DOWN)\n")	# Daten in Datei speichern
-	file.write(str(zeit) + " ; " + name_IN_03 + " ; " + str(state_03) + " (Pull-DOWN)\n")      # Daten in Datei speichern
-	file.write(str(zeit) + " ; " + name_IN_04 + " ; " + str(state_04) + " (Pull-DOWN)\n")      # Daten in Datei speichern
-	file.close()									# Datei schließen
+	file = open(logfile,"a")						# Datei öffnen
+	file.write(str(zeit) + ";" + name_IN_01 + ";" + str(state_01) + "\n")	# Daten in Datei speichern
+	file.write(str(zeit) + ";" + name_IN_02 + ";" + str(state_02) + "\n")	# Daten in Datei speichern
+	file.write(str(zeit) + ";" + name_IN_03 + ";" + str(state_03) + "\n")   # Daten in Datei speichern
+	file.write(str(zeit) + ";" + name_IN_04 + ";" + str(state_04) + "\n")   # Daten in Datei speichern
+	file.close()								# Datei schließen
 
 
 	# Alarmausgeben
@@ -102,6 +116,11 @@ def close():
 	# Abschlussprozedur
 	#------------------
 	GPIO.cleanup()							# GPIO Einstellungen zurücksetzen
+	GPIO.remove_event_detect(IN_01)					# Flankenerkennung deaktivieren
+	GPIO.remove_event_detect(IN_02)                                 # Flankenerkennung deaktivieren
+	GPIO.remove_event_detect(IN_03)                                 # Flankenerkennung deaktivieren
+	GPIO.remove_event_detect(IN_04)                                 # Flankenerkennung deaktivieren
+
 
 
 #Codeblock zum Testen bei direktem aufrufen der Datei
