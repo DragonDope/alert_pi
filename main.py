3c2de02af66511f3172fde34159892032f7f8891
DragonDope <DragonDope@ist-willig.de>
2016-09-04 16:08:18 +0200

    Now -> 8 inputs and 6 outputs

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ IN_01 = config.getint('hardware', 'pin_IN_01')
 IN_02 = config.getint('hardware', 'pin_IN_02')
 IN_03 = config.getint('hardware', 'pin_IN_03')
 IN_04 = config.getint('hardware', 'pin_IN_04')
+IN_05 = config.getint('hardware', 'pin_IN_05')
+IN_06 = config.getint('hardware', 'pin_IN_06')
+IN_07 = config.getint('hardware', 'pin_IN_07')
+IN_08 = config.getint('hardware', 'pin_IN_08')
 
 #Schlagwort Dictionary
 
@@ -26,10 +30,18 @@ name_IN_01 = config.get('name', 'name_IN_01')
 name_IN_02 = config.get('name', 'name_IN_02')
 name_IN_03 = config.get('name', 'name_IN_03')
 name_IN_04 = config.get('name', 'name_IN_04')
+name_IN_05 = config.get('name', 'name_IN_05')
+name_IN_06 = config.get('name', 'name_IN_06')
+name_IN_07 = config.get('name', 'name_IN_07')
+name_IN_08 = config.get('name', 'name_IN_08')
+
 
 OUT_01 = config.getint('hardware', 'pin_OUT_01')
 OUT_02 = config.getint('hardware', 'pin_OUT_02')
 OUT_03 = config.getint('hardware', 'pin_OUT_03')
+OUT_04 = config.getint('hardware', 'pin_OUT_04')
+OUT_05 = config.getint('hardware', 'pin_OUT_05')
+OUT_06 = config.getint('hardware', 'pin_OUT_06')
 
 logfile = config.get('alert', 'logfile')
 
@@ -43,61 +55,80 @@ GPIO.setup(IN_01, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)		# GPIO PIN einstellen
 GPIO.setup(IN_02, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)		# GPIO PIN einstellen
 GPIO.setup(IN_03, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
 GPIO.setup(IN_04, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
+GPIO.setup(IN_05, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
+GPIO.setup(IN_06, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
+GPIO.setup(IN_07, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
+GPIO.setup(IN_08, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
 GPIO.setup(OUT_01, GPIO.OUT)					# GPIO PIN einstellen
 GPIO.setup(OUT_02, GPIO.OUT)                                    # GPIO PIN einstellen
 GPIO.setup(OUT_03, GPIO.OUT)                                    # GPIO PIN einstellen
+GPIO.setup(OUT_04, GPIO.OUT)                                    # GPIO PIN einstellen
+GPIO.setup(OUT_05, GPIO.OUT)                                    # GPIO PIN einstellen
+GPIO.setup(OUT_06, GPIO.OUT)                                    # GPIO PIN einstellen
 
 
 def main():
 	
+#        GPIO.add_event_detect(IN_01, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
+#        GPIO.add_event_detect(IN_02, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
+#        GPIO.add_event_detect(IN_03, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
+#        GPIO.add_event_detect(IN_04, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
+#        GPIO.add_event_detect(IN_05, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
+#        GPIO.add_event_detect(IN_06, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
+#        GPIO.add_event_detect(IN_07, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
+#        GPIO.add_event_detect(IN_08, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
 
-        GPIO.add_event_detect(IN_01, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
-        GPIO.add_event_detect(IN_02, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
-        GPIO.add_event_detect(IN_03, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
-        GPIO.add_event_detect(IN_04, GPIO.RISING)                       # Flankenerkennung für Eingang aktivieren
 
 	while(True):							#Loop
 		# Zusänder der Eingänge abrufen
 		#------------------------------
 		zeit = time.strftime("%Y-%m-%d;%X")				# Aktuelle Systemzeit einlesen
 		
-		if GPIO.event_detected(IN_01):					# Auslöser
-			save_log(zeit, name_IN_01)				# Log eintragen
-                if GPIO.event_detected(IN_02):                                  # Auslöser
+#		if GPIO.event_detected(IN_01):					# Auslöser
+#			save_log(zeit, name_IN_01)				# Log eintragen
+#                if GPIO.event_detected(IN_02):                                  # Auslöser
+#                        save_log(zeit, name_IN_02)                              # Log eintragen
+#                if GPIO.event_detected(IN_03):                                  # Auslöser
+#                        save_log(zeit, name_IN_03)                              # Log eintragen
+#                if GPIO.event_detected(IN_04):                                  # Auslöser
+#                        save_log(zeit, name_IN_04)                              # Log eintragen
+#                if GPIO.event_detected(IN_05):                                  # Auslöser
+#                        save_log(zeit, name_IN_05)                              # Log eintragen
+#                if GPIO.event_detected(IN_06):                                  # Auslöser
+#                        save_log(zeit, name_IN_06)                              # Log eintragen
+#                if GPIO.event_detected(IN_07):                                  # Auslöser
+#                        save_log(zeit, name_IN_07)                              # Log eintragen
+#                if GPIO.event_detected(IN_08):                                  # Auslöser
+#                        save_log(zeit, name_IN_08)                              # Log eintragen
+
+                if GPIO.input(IN_01):	                                  	# Auslöser
+                        save_log(zeit, name_IN_01)                              # Log eintragen
+                if GPIO.input(IN_02):   	                               	# Auslöser
                         save_log(zeit, name_IN_02)                              # Log eintragen
-                if GPIO.event_detected(IN_03):                                  # Auslöser
+                if GPIO.input(IN_03):           	                       	# Auslöser
                         save_log(zeit, name_IN_03)                              # Log eintragen
-                if GPIO.event_detected(IN_04):                                  # Auslöser
+                if GPIO.input(IN_04):                   	               	# Auslöser
                         save_log(zeit, name_IN_04)                              # Log eintragen
+                if GPIO.input(IN_05):                           	       	# Auslöser
+                        save_log(zeit, name_IN_05)                              # Log eintragen
+                if GPIO.input(IN_06):                                  		# Auslöser
+                        save_log(zeit, name_IN_06)                              # Log eintragen
+                if GPIO.input(IN_07):                                 	 	# Auslöser
+                        save_log(zeit, name_IN_07)                              # Log eintragen
+                if GPIO.input(IN_08):                                 		# Auslöser
+                        save_log(zeit, name_IN_08)                              # Log eintragen
 
 		time.sleep(0.5)							#Wartezeit bis die Schleifen ein weiters mal durchlaufen wird
 
 
 def save_log(time, name):
 	file = open(logfile,"a")						# Datei öffnen
-	file.write(str(time) + ";" + name + "\n")				# Daten in Datei speichern
+	file.write(str(time) + ";" + name + "\r\n")				# Daten in Datei speichern
 	file.close()								# Datei schließen
 	print str(time) + " " + name + " wurde ausgelöst"
 
 
 def Result():
-
-	# Bildschirmausgabe
-	#-------------------
-	print(str(zeit)  + " ; " + name_IN_01 + " ; " + str(state_01) + " (Pull-DOWN)")		# Bildschirmausgabe
-	print(str(zeit)  + " ; " + name_IN_02 + " ; " + str(state_02) + " (Pull-DOWN)")		# Bildschirmausgabe
-	print(str(zeit)  + " ; " + name_IN_03 + " ; " + str(state_03) + " (Pull-DOWN)")            # Bildschirmausgabe
-	print(str(zeit)  + " ; " + name_IN_04 + " ; " + str(state_04) + " (Pull-DOWN)")            # Bildschirmausgabe
-
-	# Daten in Datei speichern
-	#--------------------------
-	file = open(logfile,"a")						# Datei öffnen
-	file.write(str(zeit) + ";" + name_IN_01 + ";" + str(state_01) + "\n")	# Daten in Datei speichern
-	file.write(str(zeit) + ";" + name_IN_02 + ";" + str(state_02) + "\n")	# Daten in Datei speichern
-	file.write(str(zeit) + ";" + name_IN_03 + ";" + str(state_03) + "\n")   # Daten in Datei speichern
-	file.write(str(zeit) + ";" + name_IN_04 + ";" + str(state_04) + "\n")   # Daten in Datei speichern
-	file.close()								# Datei schließen
-
 
 	# Alarmausgeben
 	#--------------
@@ -122,6 +153,11 @@ def close():
 	GPIO.remove_event_detect(IN_02)                                 # Flankenerkennung deaktivieren
 	GPIO.remove_event_detect(IN_03)                                 # Flankenerkennung deaktivieren
 	GPIO.remove_event_detect(IN_04)                                 # Flankenerkennung deaktivieren
+        GPIO.remove_event_detect(IN_05)                                 # Flankenerkennung deaktivieren
+        GPIO.remove_event_detect(IN_06)                                 # Flankenerkennung deaktivieren
+        GPIO.remove_event_detect(IN_07)                                 # Flankenerkennung deaktivieren
+        GPIO.remove_event_detect(IN_08)                                 # Flankenerkennung deaktivieren
+
 
 
 
