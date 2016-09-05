1a57a35575f77687c69727c10b37e26bb6cd3c26
DragonDope <DragonDope@ist-willig.de>
2016-09-05 22:54:14 +0200

    Add loops for checking

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import RPi.GPIO as GPIO
 import email_alarm
 from ConfigParser import SafeConfigParser
+import re
 
 
 # Einstellungen abrufen und setzen
@@ -15,33 +16,20 @@ config.read('./alert.conf')
 email = config.getboolean('alert', 'email')			# Soll eine Alarm- E-Mail versendet werden?
 telefon = config.getboolean('alert', 'telefon')			# Soll ein Alarmanruf ausgeführt werden
 
-IN_01 = config.getint('hardware', 'pin_IN_01')
-IN_02 = config.getint('hardware', 'pin_IN_02')
-IN_03 = config.getint('hardware', 'pin_IN_03')
-IN_04 = config.getint('hardware', 'pin_IN_04')
-IN_05 = config.getint('hardware', 'pin_IN_05')
-IN_06 = config.getint('hardware', 'pin_IN_06')
-IN_07 = config.getint('hardware', 'pin_IN_07')
-IN_08 = config.getint('hardware', 'pin_IN_08')
+
+# Liste mit allen Inputs
+inputs = []
+for section in config.sections():
+        if re.search('^input-\d\d$', section):
+                inputs.append(section)
+
+# Liste mit allen Outputs
+outputs = []
+for section in config.sections():
+        if re.search('^output-\d\d$', section):
+                outputs.append(section)
 
 #Schlagwort Dictionary
-
-name_IN_01 = config.get('name', 'name_IN_01')
-name_IN_02 = config.get('name', 'name_IN_02')
-name_IN_03 = config.get('name', 'name_IN_03')
-name_IN_04 = config.get('name', 'name_IN_04')
-name_IN_05 = config.get('name', 'name_IN_05')
-name_IN_06 = config.get('name', 'name_IN_06')
-name_IN_07 = config.get('name', 'name_IN_07')
-name_IN_08 = config.get('name', 'name_IN_08')
-
-
-OUT_01 = config.getint('hardware', 'pin_OUT_01')
-OUT_02 = config.getint('hardware', 'pin_OUT_02')
-OUT_03 = config.getint('hardware', 'pin_OUT_03')
-OUT_04 = config.getint('hardware', 'pin_OUT_04')
-OUT_05 = config.getint('hardware', 'pin_OUT_05')
-OUT_06 = config.getint('hardware', 'pin_OUT_06')
 
 logfile = config.get('alert', 'logfile')
 
@@ -51,20 +39,15 @@ logfile = config.get('alert', 'logfile')
 GPIO.setmode(GPIO.BOARD)       					# PIN Nummerierung per P1 RPi Board
 GPIO.setwarnings(False)         				# Warnung mehrfach Initialisierung deaktivieren
 
-GPIO.setup(IN_01, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)		# GPIO PIN einstellen
-GPIO.setup(IN_02, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)		# GPIO PIN einstellen
-GPIO.setup(IN_03, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
-GPIO.setup(IN_04, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
-GPIO.setup(IN_05, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
-GPIO.setup(IN_06, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
-GPIO.setup(IN_07, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
-GPIO.setup(IN_08, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)          # GPIO PIN einstellen
-GPIO.setup(OUT_01, GPIO.OUT)					# GPIO PIN einstellen
-GPIO.setup(OUT_02, GPIO.OUT)                                    # GPIO PIN einstellen
-GPIO.setup(OUT_03, GPIO.OUT)                                    # GPIO PIN einstellen
-GPIO.setup(OUT_04, GPIO.OUT)                                    # GPIO PIN einstellen
-GPIO.setup(OUT_05, GPIO.OUT)                                    # GPIO PIN einstellen
-GPIO.setup(OUT_06, GPIO.OUT)                                    # GPIO PIN einstellen
+
+#Inputs
+for section in inputs:
+	GPIO.setup(int(config._sections[section]["pin"]), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)		# GPIO PIN einstellen
+
+
+#Outputs
+for section in outputs:
+	GPIO.setup(int(config._sections[section]["pin"]), GPIO.OUT)					# GPIO PIN einstellen
 
 
 def main():
@@ -80,7 +63,7 @@ def main():
 
 
 	while(True):							#Loop
-		# Zusänder der Eingänge abrufen
+		# Zusände der Eingänge abrufen
 		#------------------------------
 		zeit = time.strftime("%Y-%m-%d;%X")				# Aktuelle Systemzeit einlesen
 		
@@ -101,24 +84,11 @@ def main():
 #                if GPIO.event_detected(IN_08):                                  # Auslöser
 #                        save_log(zeit, name_IN_08)                              # Log eintragen
 
-                if GPIO.input(IN_01):	                                  	# Auslöser
-                        save_log(zeit, name_IN_01)                              # Log eintragen
-                if GPIO.input(IN_02):   	                               	# Auslöser
-                        save_log(zeit, name_IN_02)                              # Log eintragen
-                if GPIO.input(IN_03):           	                       	# Auslöser
-                        save_log(zeit, name_IN_03)                              # Log eintragen
-                if GPIO.input(IN_04):                   	               	# Auslöser
-                        save_log(zeit, name_IN_04)                              # Log eintragen
-                if GPIO.input(IN_05):                           	       	# Auslöser
-                        save_log(zeit, name_IN_05)                              # Log eintragen
-                if GPIO.input(IN_06):                                  		# Auslöser
-                        save_log(zeit, name_IN_06)                              # Log eintragen
-                if GPIO.input(IN_07):                                 	 	# Auslöser
-                        save_log(zeit, name_IN_07)                              # Log eintragen
-                if GPIO.input(IN_08):                                 		# Auslöser
-                        save_log(zeit, name_IN_08)                              # Log eintragen
+		for section in inputs:
+			if GPIO.input(int(config._sections[section]["pin"])):		# Auslöser
+				save_log(zeit, config._sections[section]["name"])	# Log eintragen
 
-		time.sleep(0.5)							#Wartezeit bis die Schleifen ein weiters mal durchlaufen wird
+		time.sleep(0.5)								#Wartezeit bis die Schleifen ein weiters mal durchlaufen wird
 
 
 def save_log(time, name):
